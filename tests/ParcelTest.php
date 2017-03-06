81991d3ca3e3e8a37f46a89c31f79a941e3477a6
Unkodero <webcammax-rus@yandex.ru>
2017-03-06 15:11:04 +0300

    Update ParcelTest.php

--- tests/ParcelTest.php
+++ tests/ParcelTest.php
@@ -21,8 +21,6 @@ class ParcelTest extends \PHPUnit_Framework_TestCase
     {
         $this->resource->addParcel($this->generateParcel());
         $sended = $this->resource->send();
-        var_dump($sended);
-
 
         $this->assertArrayHasKey('awb', array_shift($sended));
     }
