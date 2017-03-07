d97894981f41592e0465cf19478da8b1c80844a5
Unkodero <webcammax-rus@yandex.ru>
2017-03-07 11:33:51 +0300

    Update API.php

--- src/EcomExpressAPI/API.php
+++ src/EcomExpressAPI/API.php
@@ -155,7 +155,7 @@ class API
         $codes = $this->getAwbNumbers(count($this->parcels));
 
         foreach ($this->parcels as &$parcel) {
-            $parcel['AWB_NUMBER'] = end($codes);
+            $parcel['AWB_NUMBER'] = (string)end($codes);
             unset($codes[count($codes)-1]);
         }
 
